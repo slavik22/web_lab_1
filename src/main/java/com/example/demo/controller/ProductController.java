1c6977628121ba0eae2bda2df82c7137f5a8644a
Yaroslav Fedyna <fedynaslava@gmail.com>
2024-09-23 11:22:30 +0300

    Initial commit with product endpoint

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @GetMapping("/products/{productId}")
+    public Product getProduct(@PathVariable int productId) {
+        return new Product(productId, productId + " name");
+    }
+}
